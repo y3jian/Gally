9fd0292772498b0b00e0096b01530006dd1e4fb5
Yolanda <yolanda.jian429@gmail.com>
2025-09-27 14:28:20 -0400

    Deleted redundant gitignore

--- myapp/app/(tabs)/_layout.tsx
+++ myapp/app/(tabs)/_layout.tsx
@@ -31,6 +31,13 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
+        name="chat"
+        options={{
+          title: 'Chat',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+        }}
+      />
+      <Tabs.Screen
         name="info"
         options={{
           title: 'info',
--- myapp/app/_layout.tsx
+++ myapp/app/_layout.tsx
@@ -16,6 +16,7 @@ export default function RootLayout() {
 
         {/* Main app */}
         <Stack.Screen name="home" options={{ headerShown: false }} />
+        
 
         {/* Modals */}
         <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
